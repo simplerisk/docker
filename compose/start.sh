8d658363fb16c95e7fe4256d07bf211b47638004
Pedro R. de Oliveira <d.ol.rod@protonmail.com>
2020-09-08 14:22:21 -0600

    Replacing password on init for Simplerisk container

--- compose/start.sh
+++ compose/start.sh
@@ -1,2 +1,4 @@
+# Replace password on config
+cat /var/www/simplerisk/includes/config.php | sed "s/DB_PASSWORD', 'simplerisk/DB_PASSWORD', '`echo $SIMPLERISK_PASS`/" > /var/www/simplerisk/includes/config.php
 # Start Apache
 /usr/sbin/apache2ctl -D FOREGROUND
