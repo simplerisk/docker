235a8e8fbde127a9fcd7ae3cfc9f1d77de6cc1b2
Pedro R. de Oliveira <d.ol.rod@protonmail.com>
2020-09-08 14:24:01 -0600

    Creating a user for all hostnames

--- compose/sql_init/99-user_creation.sh
+++ compose/sql_init/99-user_creation.sh
@@ -4,6 +4,6 @@ set -e
 echo $SIMPLERISK_PASS
 
 mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
-CREATE USER 'simplerisk'@'eyjafjallajokull' IDENTIFIED BY '${SIMPLERISK_PASS}';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER ON simplerisk.* TO 'simplerisk'@'eyjafjallajokull'; 
+CREATE USER 'simplerisk'@'%' IDENTIFIED BY '${SIMPLERISK_PASS}';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER ON simplerisk.* TO 'simplerisk'@'%'; 
 EOSQL
