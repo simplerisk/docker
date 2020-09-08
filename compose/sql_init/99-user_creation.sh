96837467f415bb4e87c5e09ce8ff6160e195d928
Pedro R. de Oliveira <d.ol.rod@protonmail.com>
2020-09-08 10:28:39 -0600

    Created init script for user creation

---
+++ compose/sql_init/99-user_creation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo $SIMPLERISK_PASS
+
+mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
+CREATE USER 'simplerisk'@'eyjafjallajokull' IDENTIFIED BY '${SIMPLERISK_PASS}';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER ON simplerisk.* TO 'simplerisk'@'eyjafjallajokull'; 
+EOSQL
