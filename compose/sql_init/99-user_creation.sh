052f63f0f4842d0658a95945f9a359a7c44a6b5e
Pedro R. de Oliveira <d.ol.rod@protonmail.com>
2020-09-08 17:38:08 -0600

    Creating random password for simplerisk user

--- compose/sql_init/99-user_creation.sh
+++ compose/sql_init/99-user_creation.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-echo $SIMPLERISK_PASS
+if [ -z $SIMPLERISK_DB_PASS ]; then
+  pwgen -cn 20 1 > /tmp/pass_simplerisk.txt
+  SIMPLERISK_DB_PASS=`cat /tmp/pass_simplerisk.txt`
+fi
 
 mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
-CREATE USER 'simplerisk'@'%' IDENTIFIED BY '${SIMPLERISK_PASS}';
+CREATE USER 'simplerisk'@'%' IDENTIFIED BY '${SIMPLERISK_DB_PASS}';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER ON simplerisk.* TO 'simplerisk'@'%'; 
 EOSQL
