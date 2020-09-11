42d1b183600a8e6e7b8264b64877597e49c5cd80
Pedro R. de Oliveira <d.ol.rod@protonmail.com>
2020-09-11 08:53:56 -0600

    Fixing specifications on print_log executions

--- simplerisk-php7.2-apache/app_setup/entrypoint.sh
+++ simplerisk-php7.2-apache/app_setup/entrypoint.sh
@@ -41,7 +41,7 @@ set_config(){
     if [ ! -z $FIRST_TIME_SETUP ]; then
         if [ -z $SIMPLERISK_DB_PASSWORD ]; then
             SIMPLERISK_DB_PASSWORD=$(pwgen -cn 20 1)
-            echo "As no password was provided and this is a first time setup, a random password has been generated ($(echo $SIMPLERISK_DB_PASSWORD))"
+            print_log "initial_setup:warn" "As no password was provided and this is a first time setup, a random password has been generated ($(echo $SIMPLERISK_DB_PASSWORD))"
         fi
         sed -i "s/\('DB_PASSWORD', '\).*\(');\)/\1$(echo $SIMPLERISK_DB_PASSWORD)\2/g" $CONFIG_PATH
     else
@@ -66,19 +66,19 @@ set_config(){
 }
 
 db_setup(){
-    print_log "info" "First time setup. Will wait..."
+    print_log "initial_setup:info" "First time setup. Will wait..."
     exec_cmd "sleep $(echo ${FIRST_TIME_SETUP_WAIT:-2O})s > /dev/null 2>&1" "FIRST_TIME_SETUP_WAIT variable is set incorrectly. Exiting."
 
-    print_log "info" "Starting database set up"
+    print_log "initial_setup:info" "Starting database set up"
 
-    print_log "info" "Downloading schema..."
+    print_log "initial_setup:info" "Downloading schema..."
     SCHEMA_FILE='/tmp/simplerisk.sql'
     exec_cmd "curl -sL https://github.com/simplerisk/database/raw/master/simplerisk-en-$(cat /tmp/version).sql > $SCHEMA_FILE" "Could not download schema from Github. Exiting."
 
     FIRST_TIME_SETUP_USER="${FIRST_TIME_SETUP_USER:-root}"
     FIRST_TIME_SETUP_PASS="${FIRST_TIME_SETUP_PASS:-root}"
 
-    print_log "info" "Applying changes to MySQL database... (MySQL error will be printed to console as guidance)"
+    print_log "initial_setup:info" "Applying changes to MySQL database... (MySQL error will be printed to console as guidance)"
     exec_cmd "mysql --protocol=socket -u $FIRST_TIME_SETUP_USER -p$FIRST_TIME_SETUP_PASS -h$SIMPLERISK_DB_HOSTNAME -P$SIMPLERISK_DB_PORT <<EOSQL
     CREATE DATABASE ${SIMPLERISK_DB_DATABASE};
     USE ${SIMPLERISK_DB_DATABASE};
@@ -87,10 +87,10 @@ db_setup(){
     GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER ON ${SIMPLERISK_DB_DATABASE}.* TO ${SIMPLERISK_DB_USERNAME}@'%';
 EOSQL" "Was not able to apply settings on database. Check error above. Exiting."
 
-    print_log "info" "Setup has been applied successfully!"
+    print_log "initial_setup:info" "Setup has been applied successfully!"
 
     if [ ! -z $FIRST_TIME_SETUP_ONLY ]; then
-        print_log "info" "Running on setup only. Container will be discarded."
+        print_log "initial_setup:info" "Running on setup only. Container will be discarded."
         exit 0
     fi
 }
