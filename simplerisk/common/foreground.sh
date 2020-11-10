d7af05107db69228deac9f27d8abf5a916b6983f
P. R. d. O <WolfangAukang@users.noreply.github.com>
2020-11-10 07:14:09 -0600

    Removing KILL and SIGKILL as they cannot be trapped (#39)

--- simplerisk/common/foreground.sh
+++ simplerisk/common/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
-trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
+trap "kill -TERM -$pgrp; exit" EXIT TERM SIGTERM SIGQUIT
 
 
 source /etc/apache2/envvars
