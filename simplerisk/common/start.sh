84c71a0b9db1d035a1ed80dc5251a8cbfb3cc399
P. R. d. O <d.ol.rod@protonmail.com>
2021-06-17 08:27:13 -0600

    Created function to run mysql commands and a main function

--- simplerisk/common/start.sh
+++ simplerisk/common/start.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
-# Start MySQL and wait 10 seconds
-/etc/init.d/mysql start
-sleep 10s
+set -eo pipefail
 
-# If MySQL hasn't already been configured
-if [ ! -f /configurations/mysql-configured ]; then
-	# Set the MySQL root password
-	mysqladmin -u root password `cat /passwords/pass_mysql_root.txt`
+run_sql_command() {
+	# $1: Password
+	# $2: Command
+	MYSQL_PWD=$1 mysql -uroot -e "$2"
+}
 
-	# Create the SimpleRisk database
-	mysql -uroot -p`cat /passwords/pass_mysql_root.txt` -e "create database simplerisk;"
+configure_db() {
+	# Start MySQL and wait 10 seconds
+	/etc/init.d/mysql start && sleep 10s
 
-	# Load the SimpleRisk database schema
-	mysql -uroot -p`cat /passwords/pass_mysql_root.txt` -e "use simplerisk; \. /simplerisk.sql"
+	# If MySQL hasn't already been configured
+	if [ ! -f /configurations/mysql-configured ]; then
+		password=$(cat /passwords/pass_mysql_root.txt)
+		# Set the MySQL root password
+		mysqladmin -u root password $password
 
-	# Set the permissions for th4e SimpleRisk database
-	mysql -uroot -p`cat /passwords/pass_mysql_root.txt` -e "CREATE USER 'simplerisk'@'localhost' IDENTIFIED BY '`cat /passwords/pass_simplerisk.txt`'"
-	mysql -uroot -p`cat /passwords/pass_mysql_root.txt` -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER ON simplerisk.* TO 'simplerisk'@'localhost'"
+		# Create the SimpleRisk database
+		run_sql_command $password "create database simplerisk;"
 
-	# Create a file so this doesn't run again
-	touch /configurations/mysql-configured
-fi
+		# Load the SimpleRisk database schema
+		#mysql -uroot -p`cat /passwords/pass_mysql_root.txt` -e "use simplerisk; \. /simplerisk.sql"
+		run_sql_command $password "use simplerisk; \. /simplerisk.sql"
 
+		# Set the permissions for th4e SimpleRisk database
+		run_sql_command $password "CREATE USER 'simplerisk'@'localhost' IDENTIFIED BY '$(cat /passwords/pass_simplerisk.txt)'"
+		run_sql_command $password "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER ON simplerisk.* TO 'simplerisk'@'localhost'"
 
-# Start Apache
-/usr/sbin/apache2ctl -D FOREGROUND
+		# Create a file so this doesn't run again
+		touch /configurations/mysql-configured
+	fi
+}
+
+_main() {
+	configure_db
+	# Start Apache
+	/usr/sbin/apache2ctl -D FOREGROUND
+}
+
+_main "$@"
