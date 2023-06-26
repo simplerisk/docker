41c1ff7325cc42e7cb12515a26b02a9c98bca3bb
P. R. d. O <d.ol.rod@tutanota.com>
2023-06-26 14:33:52 -0600

    add devshell

--- flake.nix
+++ flake.nix
@@ -1,41 +1,18 @@
 {
-  description = "Docker repository";
+  description = "virtual environments";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
-    utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs) mkShell writeShellScript docker-compose dockle;
-
-      in {
-        apps = {
-          "lint" = {
-            type = "app";
-            program = "${dockle}/bin/dockle";
-          };
-          "docker-compose" = {
-            type = "app";
-            program = "${docker-compose}/bin/docker-compose";
-          };
-          "generate-stack" = {
-            type = "app";
-            program = toString (writeShellScript "generate_stack.sh" (builtins.readFile ./generate_stack.sh));
-          };
-          "generate-simplerisk-dockerfile" = {
-            type = "app";
-            program = toString (writeShellScript "generate_dockerfile.sh" (builtins.readFile ./simplerisk/generate_dockerfile.sh));
-          };
-          "generate-simplerisk-minimal-dockerfile" = {
-            type = "app";
-            program = toString (writeShellScript "generate_dockerfile.sh" (builtins.readFile ./simplerisk-minimal/generate_dockerfile.sh));
-          };
+  outputs = { self, flake-utils, devshell, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell = let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
         };
-        devShells.default = mkShell { buildInputs = [ docker-compose dockle ]; };
-      }
-    );
+        in pkgs.devshell.mkShell { imports = [ (pkgs.devshell.importTOML ./devshell.toml) ]; };
+    });
 }
